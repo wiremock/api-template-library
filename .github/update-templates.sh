58d892c48983e14d63c6b65ed42d3108d552788d
Robert Elliot <rob@lidalia.org.uk>
2023-03-06 18:50:34 +0000

    More work on the import script

--- .github/update-templates.sh
+++ .github/update-templates.sh
@@ -2,10 +2,19 @@
 
 set -euo pipefail
 
+# shellcheck disable=SC2153
 main() {
   local wiremock_cloud_url="$WIREMOCK_CLOUD_URL"
   local wiremock_cloud_username="$WIREMOCK_CLOUD_USERNAME"
   local wiremock_cloud_api_token="$WIREMOCK_CLOUD_API_TOKEN"
+
+  updateAll "$wiremock_cloud_url" "$wiremock_cloud_username" "$wiremock_cloud_api_token"
+}
+
+updateAll() {
+  local wiremock_cloud_url=$1
+  local wiremock_cloud_username=$2
+  local wiremock_cloud_api_token=$3
 
   for dir in ../*; do
     if [[ -d "$dir" ]]; then
@@ -27,22 +36,25 @@ update() {
 
   local data; data="$(jq "{apiTemplate: { slug: .slug, name: .title, description: .description, tags: .tags, logoData: \"$logoData\"}, logoMediaType: .logoMediaType }" < "$metadata")"
 
-  echo "SENDING $data"
+  echo "Creating template: $dir via POST to $wiremock_cloud_url/v1/api-templates/public"
 
-  local apiTemplate; apiTemplate="$(curl -v \
-    -u "$wiremock_cloud_username:$wiremock_cloud_api_token" \
-    -H 'Content-Type: application/json' \
-    -d "$data" \
-    "$wiremock_cloud_url/v1/api-templates/public")"
+  local apiTemplate; apiTemplate="$( \
+    curl -fsS \
+      -u "$wiremock_cloud_username:$wiremock_cloud_api_token" \
+      -H 'Content-Type: application/json' \
+      -d "$data" \
+      "$wiremock_cloud_url/v1/api-templates/public" \
+  )"
 
-  echo "GOT apiTemplate $apiTemplate"
+  echo "Created apiTemplate $apiTemplate"
 
   local apiTemplateId; apiTemplateId="$(echo "$apiTemplate" | jq -r .apiTemplate.id)"
-  echo "GOT apiTemplateId $apiTemplateId"
 
   local stubsFile; stubsFile=$(jq -r '.stubs' < "$metadata")
 
-  curl -v \
+  echo "Setting stubs: $stubsFile for template $dir via PUT to $wiremock_cloud_url/v1/api-templates/$apiTemplateId/stubs"
+
+  curl -fsS \
     -u "$wiremock_cloud_username:$wiremock_cloud_api_token" \
     -H 'Content-Type: application/json' \
     -X PUT \
